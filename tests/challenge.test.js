1173857a89a2d5b4de0638dd9272c24feceb1215
Mehrshad Cheshm Khavari <ytmehrshad@gmail.com>
2023-10-18 12:37:40 +0330

    Update the test file

--- tests/challenge.test.js
+++ tests/challenge.test.js
@@ -1,13 +1,37 @@
 "use strict";
 
-const multiply = require("../challenge");
+const fs = require("fs");
+const path = require("path");
 
-describe("multiply", () => {
-  test("multiplies two positive numbers correctly", () => {
-    expect(multiply(2, 3)).toBe(6);
-  });
+// Define the path to the answers folder
+const answersFolderPath = path.join(__dirname, "answers");
 
-  test("multiplies positive and negative numbers correctly", () => {
-    expect(multiply(4, -2)).toBe(-8);
+// Get the list of files in the answers folder
+const userChallengeFiles = fs.readdirSync(answersFolderPath);
+
+// Find the mehrshad.js file
+const mehrshadFilePath = path.join(answersFolderPath, "mehrshad.js");
+const mehrshad = require(mehrshadFilePath);
+
+// Loop through each user file and run the tests
+userChallengeFiles.forEach((fileName) => {
+  describe(`User Challenge - ${fileName}`, () => {
+    if (fileName !== "mehrshad.js") {
+      const filePath = path.join(answersFolderPath, fileName);
+      const userMultiply = require(filePath);
+
+      // Add your test cases here
+      test("multiplies two positive numbers correctly", () => {
+        const expected = mehrshad.multiply(2, 3); // Expected answer from mehrshad.js
+        const actual = userMultiply(2, 3); // User's answer
+        expect(actual).toBe(expected);
+      });
+
+      test("multiplies positive and negative numbers correctly", () => {
+        const expected = mehrshad.multiply(4, -2); // Expected answer from mehrshad.js
+        const actual = userMultiply(4, -2); // User's answer
+        expect(actual).toBe(expected);
+      });
+    }
   });
 });
